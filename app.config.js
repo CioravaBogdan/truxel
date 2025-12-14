189f775b580fde489261eef17cea3ac145274f06
Infant.ro Team <office@infant.ro>
2025-12-14 17:47:44 +0200

    chore: bump version to 1.0.12 (build 18)

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: "Truxel",
     slug: "truxel",
-    version: "1.0.11",
+    version: "1.0.12",
     orientation: "portrait",
     icon: "./assets/Truxel_Brand/App Store 1024 x 1024.png",
     scheme: "truxel",
@@ -15,7 +15,7 @@ export default {
       backgroundColor: "#ffffff"
     },
     ios: {
-      buildNumber: "17",
+      buildNumber: "18",
       supportsTablet: false,
       bundleIdentifier: "io.truxel.app",
       infoPlist: {
@@ -38,7 +38,7 @@ export default {
       icon: "./assets/Truxel_Brand/App Store 1024 x 1024.png"
     },
     android: {
-      versionCode: 17,
+      versionCode: 18,
       package: "io.truxel.app",
       permissions: [
         "ACCESS_COARSE_LOCATION",
